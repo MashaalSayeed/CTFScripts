d441dd8054f919eaaa21aa4d78b06a785d74346b
MashaalSayeed <mashaal2403@gmail.com>
2021-07-02 15:03:31 +0400

    LICENSE and fixes

--- utils.py
+++ utils.py
@@ -1,11 +1,22 @@
 import binascii
+import struct
 
 
 def string_xor(s1, s2):
 	return ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(s1, s2))
 
-def bytes_to_int(self, b):
+
+def bytes_to_int(b):
 	return int(binascii.hexlify(b), 16)
 
-def int_to_bytes(self, num):
+
+def int_to_bytes(num):
 	return binascii.unhexlify('%x' % num)
+
+
+def little_endian(num):
+	return struct.pack('I', num)
+
+
+def big_endian(num):
+	return struct.pack('>I', num)
